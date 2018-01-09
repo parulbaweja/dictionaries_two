8b557be5bd721024ef3102afe47ac8c7ee570367
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 16:34:41 -0800

    Added user input

--- ratings.py
+++ ratings.py
@@ -1,6 +1,33 @@
-def get_ratings(filename):
-    """Restaurant rating lister."""
+import sys
 
+def new_rating():
+    """ Asks user for a new restaurant name and rating.
+    """
+
+    #stores both inputs in name and score
+    name = raw_input("Please name the restaurant you would like to rate ?")
+    score = raw_input("how would you rate the Restaurant?")
+    return name, score
+
+
+def organize_restaurants(restaurants):
+    """ accepts a dictionary of restaurants along with ratings
+        prints the restaurant name and ratings
+    """
+
+    #creates a sorted list of pairs (tuples) using items()
+    ratings = sorted(restaurants.items())
+
+    for restaurant in ratings:
+        print "{} has a rating of {}.".format(restaurant[0],restaurant[1])
+
+
+
+def get_ratings(filename):
+    """Creates a dictionary from a file of restaurants and ratings.
+        Prints the restaurants and ratings alphabetically.
+        Asks user for input using new_rating() function.
+    """
 
     restaurants = {}
 
@@ -10,10 +37,15 @@ def get_ratings(filename):
             name, rating = tuple(line.split(':'))
             restaurants[name] = restaurants.get(name, 0) + int(rating)
 
-    ratings = sorted(restaurants.items())
+    organize_restaurants(restaurants)
+    
+    response = raw_input("Would you like to rate a new restaurant? Y/N")
 
-    for restaurant in ratings:
-        print "{} has a rating of {}.".format(restaurant[0],restaurant[1])
+    if response == 'Y':
+        name, score = new_rating()
+        restaurants[name] = restaurants.get(name, 0) + int(score)
 
-get_ratings('scores.txt')
+        organize_restaurants(restaurants)
+
+get_ratings(sys.argv[1])
 
