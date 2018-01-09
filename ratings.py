ddda22c712ff4c2ed4f352cf9aaacf67203067cc
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 18:02:36 -0800

    further studies

--- ratings.py
+++ ratings.py
@@ -1,51 +1,83 @@
-import sys
-
-def new_rating():
-    """ Asks user for a new restaurant name and rating.
-    """
-
-    #stores both inputs in name and score
-    name = raw_input("Please name the restaurant you would like to rate ?")
-    score = raw_input("how would you rate the Restaurant?")
-    return name, score
+restaurants = {}
 
 
-def organize_restaurants(restaurants):
+with open('scores.txt') as all_restaurants:
+    for line in all_restaurants:
+        line = line.rstrip()
+        name, rating = tuple(line.split(':'))
+        restaurants[name] = restaurants.get(name, 0) + int(rating)
+
+
+def print_restaurants(restaurants):
     """ accepts a dictionary of restaurants along with ratings
         prints the restaurant name and ratings
     """
 
     #creates a sorted list of pairs (tuples) using items()
+
     ratings = sorted(restaurants.items())
 
-    for restaurant in ratings:
-        print "{} has a rating of {}.".format(restaurant[0],restaurant[1])
+    for name, rating in ratings:
+        print "{} has a rating of {}.".format(name, rating)
 
 
-
-def get_ratings(filename):
-    """Creates a dictionary from a file of restaurants and ratings.
-        Prints the restaurants and ratings alphabetically.
-        Asks user for input using new_rating() function.
+def prompt_new_rating():
+    """ Asks user for a new restaurant name and rating.
     """
 
-    restaurants = {}
+    #stores both inputs in name and score
+    name = raw_input("Please name the restaurant you would like to rate ?")
+    score = raw_input("How would you rate the Restaurant?")
+    return name, score
 
-    with open(filename) as all_restaurants:
-        for line in all_restaurants:
-            line = line.rstrip()
-            name, rating = tuple(line.split(':'))
-            restaurants[name] = restaurants.get(name, 0) + int(rating)
 
-    organize_restaurants(restaurants)
-    
-    response = raw_input("Would you like to rate a new restaurant? Y/N")
+def is_valid(response, x, y):
+    """Checks if response is between the input range of x and y.
+    """
 
-    if response == 'Y':
-        name, score = new_rating()
+    if response.isdigit():
+        response = int(response)
+        return response < y and response > x
+
+
+def update_rating():
+    chosen_restaurant = raw_input("Which restaurant rating would you like to update ?")
+    new_rating = raw_input("What is your new rating?")
+
+    if is_valid(new_rating, 0, 6):
+        restaurants[chosen_restaurant] = new_rating
+        print_restaurants(restaurants)
+    else:
+        print "This is not a valid input. Please try again."
+        update_rating()
+
+def execute_response(response):
+    if response == 1:
+        name, score = prompt_new_rating()
         restaurants[name] = restaurants.get(name, 0) + int(score)
+        print_restaurants(restaurants)
+    if response == 2:
+        print_restaurants(restaurants)
+    if response == 3:
+        print_restaurants(restaurants)
+        update_rating()
 
-        organize_restaurants(restaurants)
 
-get_ratings(sys.argv[1])
+while True:
+    print "What would you like to do?"
+    print "1. Rate a new restaurant"
+    print "2. View current ratings"
+    print "3. Update a current rating"
+    print "4. Quit."
 
+    response = raw_input('Your selection?')
+
+    if is_valid(response, 0, 5):
+        response = int(response)
+        execute_response(response)
+
+        if response == 4:
+            print "Thanks for using RestaurantRater! See you next time"
+            break
+    else:
+        print "That was not a valid response. Try again."
